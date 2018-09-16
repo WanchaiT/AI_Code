52124823b6d0670d87fc8e6e4079c6ef5576da6a
Wanchai Thiantanawat <wanchairoot@WanMac.local>
2018-09-16 22:56:00 +0700

    Add comment list set

--- TestDFS.py
+++ TestDFS.py
@@ -21,12 +21,13 @@ class Queue:
         print(self.items)
 
 def find_next_state(set ,state) :
-    north_side = state[0]
+    north_side = state[0]   #list[set{}]
     south_side = state[1]
     list_state = []
 
     #set_side is dictionry
     set_side = {"north" : north_side ,"south" : south_side}
+    #dict{"n" : list[set{}] ,"s" : list[sets{}]}
 
     if "AS" in set_side["north"] :  #"AS" on north_side
         childs_of_north = childs_in_side("north" ,"south" ,set_side)
@@ -36,6 +37,8 @@ def find_next_state(set ,state) :
             not(list(set_side["north"] ,set_side["south"]) in set) :
             list_state = list(set_side["north"] ,set_side["south"])
 
+        for state in list_state :
+
     else :                          #"AS" on south_side
         childs_of_south = childs_in_side("south" ,"north" ,set_side)
 
@@ -44,13 +47,24 @@ def find_next_state(set ,state) :
             not(list[set_side["north"] ,set_side["south"]] in set) :
             list_state = list(set_side["north"] ,set_side["south"])
 
-def childs_in_side(side_add ,side_remove ,set_side) :
-    set_side[side_add].add("AS")
-    set_side[side_remove].remove("AS")
-    return childs_of_side = list(set_side[side_add])
+        for state in list_state :
+
+def childs_in_side(side_insert ,side_remove ,set_side) :
+    set_side[side_insert].insert({"AS"})
+    set_side[side_remove].remove({"AS"})
+    return childs_of_side = list(set_side[side_add])    #list[set{}]
 
 def check_state(state) :
+    go_and_fo = {"Go" ,"Fo"}
+    gr_and_go = {"Gr" ,"Go"}
+    only_as = {"As"}
 
+    north_side = state["north"] #list[set{}]
+    south_side = state["south"]
+
+    '''return not((go_and_fo in north_side) and (only_as in north_side)
+                or (gr_and_go in south_side) and (gr_and_go in south_side)
+                or ())'''
 
 def is_goal(state):
     return state[0] == {}
@@ -60,31 +74,34 @@ def find_path(queue, set) :
         return False
     else :
         current_path = queue.dequeue() #current_path  = last queue
+        #list[list[set{} ,set{}]]
 
     current_state = current_path[-1]    #current_path[-1] = last_state
+    #list[set{} ,set{}]
 
     if is_goal(current_state) :
         return current_path #return path of goal
     else :
         list_next_state = findNextState(set ,current_state)
+        #list[list[set{} ,set{}]]
 
         #ex list_next_state = [[{"a"},{"b"}] , [{"c"},{"d"}]]
         #ex state = next_state[0] = [{"a"},{"b"}]  --> state = [next_state[0]]
         #set = [[{"a"},{"b"}] , [{"c"},{"d"}]]
 
         for state in list_next_state :
-            set += [state]
+            set += [state]  #list[list[set{} ,set{}]]
             next_path = current_path + [state]
             queue.add(next_path)
 
         return find_path(queue, set)
 
 queue = Queue()
-first_state = [set({"As","Fo","Go","Gr"}) ,set()]
+first_state = [{"As","Fo","Go","Gr"} ,{}]   #list[set{} ,set{}]
 
 #implement a set of type list, and inside list have states
 #and inside states have sets
-set = [first_state]
+set = [first_state] #list[list[set{} ,set{}]]
 queue.add(set)
 
 #path = [[{"a","b"},{"b"}{"c"}] , [{"a"},{"b","c","d"}] , ...]
